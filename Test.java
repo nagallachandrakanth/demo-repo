5d40cd8aeb4c41f9f63d55e0ad5489994e1c96ee
nagallachandrakanth <113973309+nagallachandrakanth@users.noreply.github.com>
2022-09-20 16:18:35 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ class Test{
 public static void main(String[] args){
 system.out.println("test2 is completed");
 system.out.println("test3 also completed");
+system.out.println("cloning practice");
 }
 }
